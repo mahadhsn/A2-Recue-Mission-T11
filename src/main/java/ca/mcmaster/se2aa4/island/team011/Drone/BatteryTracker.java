f5bb37a248b6f67029a8fb55246c66aa87aadc3e
Kylie Gun <kyliegun0@gmail.com>
2025-03-21 18:52:05 -0400

    Closes #9 Updating cost and battery usage

--- src/main/java/ca/mcmaster/se2aa4/island/team011/Drone/BatteryTracker.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team011/Drone/BatteryTracker.java
@@ -2,10 +2,14 @@ package ca.mcmaster.se2aa4.island.team011.Drone;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import java.io.StringReader;
 
 public class BatteryTracker {
     private static final Logger logger = LogManager.getLogger(BatteryTracker.class);
     private int batteryLevel; //current battery level of drone
+    private BatteryTrackListener listener;
 
     public BatteryTracker(int batteryLevel){
         if (batteryLevel < 0){
@@ -16,6 +20,16 @@ public class BatteryTracker {
 
     }
 
+    public void setListener(BatteryTrackListener listener){
+        this.listener = listener;
+    }
+
+    public int getCost (String s){
+        JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+        //Integer cost = response.getInt("cost");
+        return response.getInt("cost");
+    }
+
     public boolean hasEnoughBattery(int cost){
         return batteryLevel >= cost;
     }
@@ -24,6 +38,9 @@ public class BatteryTracker {
         if (hasEnoughBattery(cost)){
             batteryLevel -= cost;
             logger.info("Battery level reduced by {}. Remaining: {}", cost, batteryLevel);
+            if (listener != null){
+                listener.onBatteryUpdate(batteryLevel);
+            }
         } else{
             logger.info("Not enough battery to perform this action. {} units remaining.", batteryLevel);
         }
